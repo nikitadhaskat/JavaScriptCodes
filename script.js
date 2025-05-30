27ab6e5c21ac2707057ecd23e9b8df5655bb7d1d
Nikita Dhaskat <nikitadhaskat2002@gmail.com>
2025-05-30 18:03:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+let str = "I am learning Java"
+console.log(str.length)
+console.log(str.charAt(1))
+console.log(str.concat(" Programming"))
+console.log(str.indexOf("l"))
+console.log(str.substring(2))
+console.log(str.split("am"))
+console.log(str.endsWith("Java"))
+console.log(str.startsWith("I"))
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+
+/*let student = {
+    studname = "Nikita",
+    age = 23,
+    city = "Pune";
+}
+console.log(student);
+console.log(student.studname)*/
